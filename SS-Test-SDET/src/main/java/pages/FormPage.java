96acec575356d15ce4bf1dbf31b534b805eaf86f
F.Musharyapov <f.musharyapov@gmail.com>
2025-02-23 14:25:44 +0400

    Исправление ошибки компиляции, реализация всех локаторов и методов для отправки формы

--- SS-Test-SDET/src/main/java/pages/FormPage.java
+++ SS-Test-SDET/src/main/java/pages/FormPage.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.io.File;
+
 /**
  * Класс в котором происходит взаимодействие со страннице с формой
  */
@@ -29,6 +31,68 @@ public class FormPage {
     @FindBy(css = "input[id='firstName']")
     private WebElement firstName;
 
+    @FindBy(css = "input[id='lastName']")
+    private WebElement lastName;
+
+    @FindBy(css = "input[id='userEmail']")
+    private WebElement userEmail;
+
+    @FindBy(css = "label[for='gender-radio-3']")
+    private WebElement gender;
+
+    @FindBy(css = "input[id='userNumber']")
+    private WebElement userNumber;
+
+    @FindBy(id = "dateOfBirthInput")
+    private WebElement dateOfBirth;
+
+    @FindBy(xpath = "//*[@id=\"dateOfBirth\"]/div[2]/div[2]/div/div/div[2]/div[1]/div[2]/div[1]/select")
+    private WebElement month;
+
+    @FindBy(xpath = "//*[@id=\"dateOfBirth\"]/div[2]/div[2]/div/div/div[2]/div[1]/div[2]/div[1]/select/option[10]")
+    private WebElement monthOctober;
+
+    @FindBy(xpath = "//*[@id=\"dateOfBirth\"]/div[2]/div[2]/div/div/div[2]/div[1]/div[2]/div[2]/select")
+    private WebElement year;
+
+    @FindBy(xpath = "//*[@id=\"dateOfBirth\"]/div[2]/div[2]/div/div/div[2]/div[1]/div[2]/div[2]/select/option[92]")
+    private WebElement year1991;
+
+    @FindBy(xpath = "//*[@id=\"dateOfBirth\"]/div[2]/div[2]/div/div/div[2]/div[2]/div[3]/div[1]")
+    private WebElement day13;
+
+    @FindBy(id = "subjectsInput")
+    private WebElement subjectsInput;
+
+    @FindBy(id = "react-select-2-option-0")
+    private WebElement subjectsSelect;
+
+    @FindBy(xpath = "//*[@id=\"hobbiesWrapper\"]/div[2]/div[1]/label")
+    private WebElement hobbiesWrapper;
+
+    @FindBy(id = "uploadPicture")
+    private WebElement uploadPicture;
+
+    @FindBy(css = "textarea[id='currentAddress']")
+    private WebElement currentAddress;
+
+    @FindBy(id = "state")
+    private WebElement selectState;
+
+    @FindBy(id = "react-select-3-option-0")
+    private WebElement selectStateNCR;
+
+    @FindBy(id = "city")
+    private WebElement selectCity;
+
+    @FindBy(id = "react-select-4-option-0")
+    private WebElement selectCityDelhi;
+
+    @FindBy(id = "submit")
+    private WebElement submitButton;
+
+
+
     /**
      * Конструктор создания FormPage
      *
@@ -42,13 +106,69 @@ public class FormPage {
     /**
      * Метод ввода текста в поле поиска
      *
-     * @param str текст ввода
+     * @param input текст ввода
      * @return текущая страница
      */
     @Step("Ввод данных в поле ввода")
-    public FormPage inputFirstName(String str) {
-        firstName.sendKeys(str);
+    public FormPage inputFirstName(String input) {
+        firstName.sendKeys(input);
         return this;
     }
 
+    public FormPage inputLastName(String input) {
+        lastName.sendKeys(input);
+        return this;
+    }
+    public FormPage inputUserEmail(String input) {
+        userEmail.sendKeys(input);
+        return this;
+    }
+    public FormPage clickToGender() {
+        gender.click();
+        return this;
+    }
+    public FormPage inputUserNumber(String input) {
+        userNumber.sendKeys(input);
+        return this;
+    }
+    public FormPage selectDateOfBirth() {
+        dateOfBirth.click();
+        month.click();
+        monthOctober.click();
+        year.click();
+        year1991.click();
+        day13.click();
+        return this;
+    }
+    public FormPage inputSubjectsInput(String input) {
+        subjectsInput.sendKeys(input);
+        subjectsSelect.click();
+        return this;
+    }
+    public FormPage clickToHobbiesWrapper() {
+        hobbiesWrapper.click();
+        return this;
+    }
+    public FormPage uploadPicture() {
+        uploadPicture.sendKeys(new File(config.testPicture()).getAbsolutePath());
+        return this;
+    }
+    public FormPage inputCurrentAddress(String input) {
+        currentAddress.sendKeys(input);
+        return this;
+    }
+    public FormPage clickToSelectState() {
+        selectState.click();
+        selectStateNCR.click();
+        return this;
+    }
+    public FormPage clickToSelectCity() {
+        selectCity.click();
+        selectCityDelhi.click();
+        return this;
+    }
+    public FormPage clickToSubmit() {
+        submitButton.click();
+        return this;
+    }
 }
